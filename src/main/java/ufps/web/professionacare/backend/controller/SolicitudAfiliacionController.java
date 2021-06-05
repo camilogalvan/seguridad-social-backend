f7ad98789cff278a9cb19324023a22959da11181
OMAR RAMON <omarmontes.879@gmail.com>
2021-06-05 11:00:10 -0500

    detalles en controlador

--- src/main/java/ufps/web/professionacare/backend/controller/SolicitudAfiliacionController.java
+++ src/main/java/ufps/web/professionacare/backend/controller/SolicitudAfiliacionController.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +25,17 @@ public class SolicitudAfiliacionController {
 		return serv.Get();
 	}
 	
-	@GetMapping("/FindById")
-	public SsptSolicitudAfiliacion GetId(int id) {
+	@GetMapping("/FindById/{id}")
+	public SsptSolicitudAfiliacion GetId1(@PathVariable int id) {
+		System.out.println("id1:"+ id);
 		
-		return serv.GetPorId(id);
+		return null; //serv.GetPorId(id);
+	}
+	
+	@GetMapping("/FindById")
+	public SsptSolicitudAfiliacion GetId2(@RequestBody int id) {
+		System.out.println("id2:"+ id);
+		return null; //serv.GetPorId(id);
 	}
 	
 	@GetMapping("/save")
