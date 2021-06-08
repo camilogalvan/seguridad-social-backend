5c49156d525e4321065bfbf34d0f7ec2afea9a16
juanse50 <juansebastianao@ufps.edu.co>
2021-06-08 00:53:11 -0500

    se agregaron algunos metodos de cliente y solicitud

---
+++ src/main/java/ufps/web/professionacare/backend/container/SolicitudEntrada.java
@@ -0,0 +1,47 @@
+package ufps.web.professionacare.backend.container;
+
+public class SolicitudEntrada {
+	
+	public String observaciones;
+	
+	public String NombrePlan;
+	
+	public String cliente;
+	
+	public String usuario;
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public String getNombrePlan() {
+		return NombrePlan;
+	}
+
+	public void setNombrePlan(String nombrePlan) {
+		NombrePlan = nombrePlan;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+}
