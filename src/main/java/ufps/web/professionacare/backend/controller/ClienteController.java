768fa542a5002d3143293feedca18cff5e8e3ad7
OMAR RAMON <omarmontes.879@gmail.com>
2021-06-08 22:02:34 -0500

    Render código

--- src/main/java/ufps/web/professionacare/backend/controller/ClienteController.java
+++ src/main/java/ufps/web/professionacare/backend/controller/ClienteController.java
@@ -3,24 +3,16 @@ package ufps.web.professionacare.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import ufps.web.professionacare.backend.container.ClienteApi;
 import ufps.web.professionacare.backend.container.ClienteEntrada;
-import ufps.web.professionacare.backend.container.EmpresaApi;
-import ufps.web.professionacare.backend.container.EmpresaEntradaApi;
 import ufps.web.professionacare.backend.enums.EstadoCliente;
 import ufps.web.professionacare.backend.model.SsptCliente;
-import ufps.web.professionacare.backend.model.SsptEmpresa;
-import ufps.web.professionacare.backend.model.SsptFile;
 import ufps.web.professionacare.backend.model.SsptMunicipio;
 import ufps.web.professionacare.backend.model.SsptTipoCliente;
 import ufps.web.professionacare.backend.model.SsptTipoIdentificacion;
@@ -28,42 +20,33 @@ import ufps.web.professionacare.backend.service.SsptClienteService;
 import ufps.web.professionacare.backend.service.SsptMunicipioService;
 import ufps.web.professionacare.backend.service.SsptTipoClienteService;
 import ufps.web.professionacare.backend.service.SsptTipoIdentificacionService;
-import ufps.web.professionacare.backend.enums.EstadoCliente;
+
 @RestController
 @RequestMapping("/api/clientes/")
 public class ClienteController {
-	
+
 	@Autowired
 	private SsptClienteService service;
-	
+
 	@Autowired
 	private SsptTipoIdentificacionService tipoIdentificacionService;
-	
+
 	@Autowired
 	private SsptTipoClienteService tipoClienteService;
-	
-	
-	@Autowired 
+
+	@Autowired
 	private SsptMunicipioService municipioser;
-	
+
 	@GetMapping("todos")
 	public List<SsptCliente> getAll() {
-		
+
 		return service.Get();
-		
+
 	}
-	
-	/**@PostMapping("save")
-	@ResponseStatus(HttpStatus.CREATED)
-	public SsptCliente save(@RequestBody SsptClienteEntrada cliente) {
-		
-		cliente.prePersist();
-		return service.guardar(cliente);
-	}**/
-	
+
 	@PostMapping(value = "save")
 	public SsptCliente save(@RequestBody ClienteEntrada entrada) {
-		//ClienteApi api = new ClienteApi();
+		// ClienteApi api = new ClienteApi();
 
 		SsptTipoIdentificacion tipoId = tipoIdentificacionService.buscarPorId(entrada.getIdTipoIdentificacion());
 		SsptTipoCliente tipoCliente = tipoClienteService.buscarPorId(entrada.getIdTipoCliente());
@@ -72,9 +55,9 @@ public class ClienteController {
 		if (tipoId == null || tipoCliente == null) {
 			return null;
 		}
-		
+
 		SsptCliente client = new SsptCliente();
-		
+
 		client.setNombre1(entrada.getNombre1());
 		client.setNombre2(entrada.getNombre2());
 		client.setApellido1(entrada.getApellido1());
@@ -84,73 +67,33 @@ public class ClienteController {
 		client.setDireccion(entrada.getDireccion());
 		client.setIdentificacion(entrada.getIdentificacion());
 
-		client.setTipoIdentificacion(tipoId);	
-		client.setTipoCliente(tipoCliente);	
+		client.setTipoIdentificacion(tipoId);
+		client.setTipoCliente(tipoCliente);
 		client.setMunicipio(municipio);
 
 		return service.guardar(client);
 	}
-	
-//	@PostMapping("save")
-//	public SsptCliente save(@RequestBody ClienteEntrada entrada) {
-//		
-//
-//		SsptCliente client = new SsptCliente();
-//		
-//		SsptTipoIdentificacion tipoId = tipoIdentificacionService.buscarPorId(entrada.getIdTipoIdentificacion());
-//		
-//		if (tipoId == null) {
-//			return null;
-//		}
-//
-//		client.setNombre1(entrada.getNombre1());
-//		client.setNombre2(entrada.getNombre2());
-//		client.setApellido1(entrada.getApellido1());
-//		client.setApellido2(entrada.getApellido2());
-//		client.setCorreo(entrada.getCorreo());
-//		client.setTelefono(entrada.getTelefono());
-//		client.setDireccion(entrada.getDireccion());
-//		client.setIdentificacion(entrada.getIdentificacion());
-//		
-//		
-//		
-////		client.setTipoIdentificacion(new SsptTipoIdentificacion("cedula de ciudadania", "CC"));
-//		//client.setTipoCliente(new SsptTipoCliente(entrada.getTipoCliente(), "Un cliente despendfiente" ));
-////		client.prePersist();
-//
-//		
-//
-//		//client = service.save(client);
-//
-//		//api.setEmpresa(proveedorSearch);
-//
-//		return service.guardar(client);
-//	}
+
 	@GetMapping("porId/{id}")
 	public SsptCliente GetPorId(@PathVariable int id) {
-		
 		return service.GetPorId(id);
-		
 	}
-	
+
 	@GetMapping("porCedula/{cedula}")
 	public SsptCliente GetPorCedula(@PathVariable String cedula) {
-		
 		return service.GetPorCedula(cedula);
 	}
-	
-	
+
 	@GetMapping("porEstado")
 	public SsptCliente GetPorEstado(@RequestBody EstadoCliente e) {
-		
 		return service.GetPorEstado(e);
 	}
-	
+
 	@PostMapping("cambiarEstado/{id}/{estado}")
 	public SsptCliente CambiarEstado(@PathVariable int id, @PathVariable String estado) {
-		SsptCliente cli = this.GetPorId(id);		
-		cli.setEstadoCliente(EstadoCliente.valueOf(estado));		
-		return service.guardar(cli);		
+		SsptCliente cli = this.GetPorId(id);
+		cli.setEstadoCliente(EstadoCliente.valueOf(estado));
+		return service.guardar(cli);
 	}
 
 }
