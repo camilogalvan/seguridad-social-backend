de076ae8a769e18711700183bf33b375ee8ca4bf
OMAR RAMON <omarmontes.879@gmail.com>
2021-06-16 21:47:07 -0500

    Actualizar empresa

--- src/main/java/ufps/web/professionacare/backend/enums/EstadoOrden.java
+++ src/main/java/ufps/web/professionacare/backend/enums/EstadoOrden.java
@@ -1,5 +1,20 @@
 package ufps.web.professionacare.backend.enums;
 
-public enum EstadoOrden {
-	PENDIENTE, PAGADA
+public enum EstadoOrden {	
+	PENDIENTE("Pendiente"), 
+	PAGADA("Pagada");
+
+	private String nombre;
+
+	private EstadoOrden(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
